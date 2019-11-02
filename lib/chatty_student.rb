957b2d78eece22da01060e735eb42c6c8a351cd0
Nicholas Ng <nicholasykng@gmail.com>
2019-11-02 21:51:37 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -4,8 +4,7 @@ class ChattyStudent < Student
     puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end
   def raise_hand
-    counter = 0
-    while counter < 11 super
+    super
     puts "Pick me!"
     counter += 1
   end
